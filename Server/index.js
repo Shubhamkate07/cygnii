67feed32a97ea21dd311af76054ba01c404c621d
Shubhamkate07 <123533581+Shubhamkate07@users.noreply.github.com>
2025-01-20 13:25:59 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -52,7 +52,7 @@ app.post('/submit', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5001; 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
